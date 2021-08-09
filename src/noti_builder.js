b4ad6e665ad269c27838a1d94daba6cd09ee3023
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-08 23:06:24 -0400

    create builder variables

--- src/noti_builder.js
+++ src/noti_builder.js
@@ -1,7 +1,36 @@
 
-const positon;
+let positon;
+let sizeX,sizeY;
+let x,y;
 
 if(!positon)
 {
     console.log("Noti.js says: Notification position unspecified")
+}
+
+switch(positon)
+{
+    case "top-left":
+        positon="top-left";
+        x=0;
+        y=0;
+    break;
+    
+    case "top-right":
+        positon="top-left";
+        y=0;
+        x=
+    break;
+    
+    case "bottom-left":
+        positon="top-left";
+    break;
+    
+    case "bottom-right":
+        positon="top-left";
+    break;
+
+    case "bottom-right":
+        positon="top-left";
+    break;
 }
